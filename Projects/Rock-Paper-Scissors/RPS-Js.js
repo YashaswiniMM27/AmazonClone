0cc746c4067669f911122700030a37a438d9b6dc
Yashaswini M M <yashaswini.m@jiraaf.com>
2024-12-15 21:54:22 +0530

    Rock Paper Scissors modified game

--- Projects/Rock-Paper-Scissors/RPS-Js.js
+++ Projects/Rock-Paper-Scissors/RPS-Js.js
@@ -1,59 +1,68 @@
-const score = JSON.parse(localStorage.getItem('score')) || {wins: 0, losses: 0, ties: 0};
+const score = JSON.parse(localStorage.getItem('score')) || { wins: 0, losses: 0, ties: 0 };
 
-            updateResult();
-            updateMoves();
-            updateScore();
+updateScore();
 
-            function updateScore(){
-                document.querySelector('.js-score').innerHTML = `\nWins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
-            }
+function updateScore() {
+    document.querySelector('.js-score').innerHTML = `
+        Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
 
-        function playGame(playerMove) {
-            const randomNumber = Math.random();
-            let computerMove = '';
-            let result = '';
+function updateResult(result) {
+    document.querySelector('.js-result').innerHTML = result;
+}
 
-            if (randomNumber >= 0 && randomNumber < 1 / 3) {
-                computerMove = 'Rock';
-            } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-                computerMove = 'Paper';
-            } else {
-                computerMove = 'Scissors';
-            }
+function updateMoves(playerMove, computerMove) {
+    if (!playerMove || !computerMove) {
+        document.querySelector('.js-moves').innerHTML = '';
+    } else {
+        document.querySelector('.js-moves').innerHTML = `
+            You picked <img style="height: 40px" src ='/Images/${playerMove}-emoji.png'>. Computer picked <img style="height: 40px" src ='/Images/${computerMove}-emoji.png'>.`;
+    }
+}
 
-            if (playerMove === computerMove) {
-                result = 'Tie.';
-            } else if (
-                (playerMove === 'Rock' && computerMove === 'Scissors') ||
-                (playerMove === 'Paper' && computerMove === 'Rock') ||
-                (playerMove === 'Scissors' && computerMove === 'Paper')
-            ) {
-                result = 'You win.';
-            } else {
-                result = 'You lose.';
-            }
+function playGame(playerMove) {
+    const randomNumber = Math.random();
+    let computerMove = '';
+    let result = '';
 
-            if(result === 'You win.'){
-                score.wins++;
-            }
-            else if(result === 'You lose.'){
-                score.losses++;
-            }
-            else if (result === 'Tie.'){
-                score.ties++
-            }
+    if (randomNumber < 1 / 3) {
+        computerMove = 'Rock';
+    } else if (randomNumber < 2 / 3) {
+        computerMove = 'Paper';
+    } else {
+        computerMove = 'Scissors';
+    }
 
-            localStorage.setItem('score', JSON.stringify(score));
-    
-            function updateResult(){
-                document.querySelector('.js-result').innerHTML = result;
-            }
+    if (playerMove === computerMove) {
+        result = 'Tie.';
+        score.ties++;
+    } else if (
+        (playerMove === 'Rock' && computerMove === 'Scissors') ||
+        (playerMove === 'Paper' && computerMove === 'Rock') ||
+        (playerMove === 'Scissors' && computerMove === 'Paper')
+    ) {
+        result = 'You win.';
+        score.wins++;
+    } else {
+        result = 'You lose.';
+        score.losses++;
+    }
 
-            function updateMoves(){
-                document.querySelector('.js-moves').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}.`;
-            }
+    localStorage.setItem('score', JSON.stringify(score));
 
-            updateResult();
-            updateMoves();
-            updateScore();
-        }
+    updateResult(result);
+    updateMoves(playerMove, computerMove);
+    updateScore();
+}
+
+function resetScore() {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+
+    localStorage.removeItem('score');
+
+    updateScore();
+    updateResult('');
+    updateMoves('', '');
+}
