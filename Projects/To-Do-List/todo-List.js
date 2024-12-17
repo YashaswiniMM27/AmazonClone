8fb5ac9a8b2473d6e513003e52a24156d733944f
Yashaswini M M <yashaswini.m@jiraaf.com>
2024-12-18 00:42:30 +0530

    To do List and more array exercises

---
+++ Projects/To-Do-List/todo-List.js
@@ -0,0 +1,61 @@
+const initialTodo = JSON.parse(localStorage.getItem('todo')) || [];
+const todoList = initialTodo;
+
+        function keyDownEnterEvent(){
+            if(event.key === 'Enter')
+            {
+                addTodo();
+            }
+        }
+
+        renderTodoList();
+
+        function renderTodoList(){
+            let todoHTML = '';
+
+            for(let i = 0; i < todoList.length; i++){
+                const todo = todoList[i];
+                const {name, dueDate} = todo;
+                const html = `<div style="padding-left:5px">• ${name}</div>
+                                <div style="padding-left:5px">${dueDate}</div>
+                                <div>
+                                    <button class="deleteBTN" onclick="deleteTodo(${i})">Delete</button>
+                                </div>`;
+                                
+                todoHTML += html;
+            }
+
+            document.querySelector('.js-listTodo').innerHTML = todoHTML;
+        }
+
+        function deleteTodo(index) {
+            todoList.splice(index, 1);
+            localStorage.setItem('todo', JSON.stringify(todoList));
+            renderTodoList();
+        }
+        
+
+        function addTodo(){
+            const errorElement = document.querySelector('.js-error');
+
+            const inputElement = document.querySelector('.js-input');
+            const dateInputElement = document.querySelector('.js-datePicker');
+            const name = inputElement.value;
+            const dueDate = dateInputElement.value;
+
+            errorElement.innerHTML = '';
+
+            if (name === '' || dueDate === '') {
+                errorElement.innerHTML = 'Please fill out both the task and due date.';
+                return;
+            }
+
+            todoList.push({name, dueDate});
+
+            localStorage.setItem('todo', JSON.stringify(todoList));
+
+            renderTodoList();
+
+            inputElement.value = '';
+            dateInputElement.value = '';
+        }
